d6ff32007233355d302d0c07ee0c7a6f4b553e34
Ana Beatriz <biamaiaribeiro@gmail.com>
2023-05-10 11:25:53 -0300

    cor

--- Background_Color/script.js
+++ Background_Color/script.js
@@ -1,9 +1,11 @@
 const fundo = document.querySelector("body");
 const cor = document.querySelector("input");
 
+// Definindo a cor constante de fundo antes da cor ser selecionada
 const colorpadrao = "#fff";
 fundo.style.backgroundColor = `${colorpadrao}`;
 
+// Definindo a cor que foi escolida
 function onChangeBg() {
   fundo.style.backgroundColor = `${cor.value}`;
 }
