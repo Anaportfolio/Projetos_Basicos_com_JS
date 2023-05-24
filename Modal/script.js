46732ff4b9fa9ca9895d10c4c36912e98a6270cd
Ana Beatriz <biamaiaribeiro@gmail.com>
2023-05-24 09:06:51 -0300

    modal

--- Modal/script.js
+++ Modal/script.js
@@ -1,9 +1,12 @@
-const modal = document.querySelector(".conteiner");
+//Capturando as propiedades do elemento identificado
+const modal = document.querySelector(".container");
 
+// Função para mostrar o Modal
 function Abrir() {
   modal.classList.add("active");
 }
 
+// Função para fechar o Modal
 function Fechar() {
   modal.classList.remove("active");
 }
